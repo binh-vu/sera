c0e3bae2a778ceb495792659e19e0d5de17ff1bf
Binh Vu <binh@toan2.com>
2025-09-22 07:32:47 +0700

    wip

--- web/components/src/misc/Search.tsx
+++ web/components/src/misc/Search.tsx
@@ -1,5 +1,9 @@
 import { TextInput } from "@mantine/core";
-import { IconAdjustmentsHorizontal, IconSearch } from "@tabler/icons-react";
+import {
+  IconAdjustmentsHorizontal,
+  IconKeyboard,
+  IconSearch,
+} from "@tabler/icons-react";
 import { useMemo, useState } from "react";
 
 import { debounce } from "throttle-debounce";
@@ -13,33 +17,45 @@ interface SearchInputProps {
   debounceTime?: number;
 }
 
+const keyboardIcon = <IconKeyboard size={16} stroke={1.5} />;
+const searchIcon = <IconSearch size={16} stroke={1.5} />;
+
 export const SearchInput = ({
   query,
   onChange,
   debounceTime = 250,
 }: SearchInputProps) => {
   const [value, setValue] = useState(query);
+  const [isEntering, setIsEntering] = useState(false);
 
   const debouncedOnChange = useMemo(() => {
     return debounce(
       debounceTime,
       (query: string) => {
+        setIsEntering(false);
         onChange(query);
       },
       { atBegin: false }
     );
   }, [debounceTime, onChange]);
 
+  const updateValue = useMemo(() => {
+    return (query: string) => {
+      setValue(query);
+      setIsEntering(true);
+    };
+  }, [setValue, setIsEntering]);
+
   return (
     <TextInput
       flex={1}
       label="Search"
       placeholder="Search..."
-      leftSection={<IconSearch size={16} stroke={1.5} />}
+      leftSection={isEntering ? keyboardIcon : searchIcon}
       rightSection={<IconAdjustmentsHorizontal size={16} stroke={1.5} />}
       value={value}
       onChange={(e) => {
-        setValue(e.target.value);
+        updateValue(e.target.value);
         debouncedOnChange(e.target.value);
       }}
     />
