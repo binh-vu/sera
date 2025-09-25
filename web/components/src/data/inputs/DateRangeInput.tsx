e3fe1275d44c190e3254dd2fe5a1f71b6b64389c
Binh Vu <binh@toan2.com>
2025-09-25 08:04:48 +0700

    wip

--- web/components/src/data/inputs/DateRangeInput.tsx
+++ web/components/src/data/inputs/DateRangeInput.tsx
@@ -1,30 +1,41 @@
 import dayjs from "dayjs";
 import { InputInterface } from ".";
 import { DatePickerInput } from "@mantine/dates";
+import { Group } from "@mantine/core";
 
 export const DateRangeInput: React.FC<
   InputInterface<{ from?: Date; to?: Date } | undefined>
 > = ({ property, value, onChange }) => {
   return (
-    <DatePickerInput
-      id={property.tsName}
-      type="range"
-      value={[value?.from || null, value?.to || null]}
-      onChange={(dateRange) => {
-        onChange({
-          from:
-            dateRange[0] === null
-              ? undefined
-              : dayjs(dateRange[0]).startOf("day").toDate(),
-          to:
-            dateRange[1] === null
-              ? undefined
-              : dayjs(dateRange[1]).endOf("day").toDate(),
-        });
-      }}
-      placeholder="Pick date range"
-      clearable={true}
-      popoverProps={{ withinPortal: false }}
-    />
+    <Group gap="sm" justify="space-between" grow={true}>
+      <DatePickerInput
+        id={property.tsName}
+        value={value?.from || null}
+        onChange={(date) => {
+          onChange({
+            from:
+              date === null ? undefined : dayjs(date).startOf("day").toDate(),
+          });
+        }}
+        valueFormat="YYYY MMM DD"
+        placeholder="Pick date range"
+        clearable={true}
+        popoverProps={{ withinPortal: false }}
+      />
+      <DatePickerInput
+        id={property.tsName}
+        value={value?.from || null}
+        onChange={(date) => {
+          onChange({
+            from:
+              date === null ? undefined : dayjs(date).startOf("day").toDate(),
+          });
+        }}
+        valueFormat="YYYY MMM DD"
+        placeholder="Pick date range"
+        clearable={true}
+        popoverProps={{ withinPortal: false }}
+      />
+    </Group>
   );
 };
